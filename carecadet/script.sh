bf61f2cb7a50dde6277dc95a51568101d06b649d
= <=>
2023-01-27 11:09:44 +0530

    script updated

--- carecadet/script.sh
+++ carecadet/script.sh
@@ -2,6 +2,6 @@
 
 docker stop healthlens_react_app
 docker rm healthlens_react_app
-docker rmi healthlens_react_app --force
+# docker rmi healthlens_react_app --force
 docker build -t healthlens_react_app .
 docker run -p 5008:3000 -d --name healthlens_react_app --restart always healthlens_react_app
