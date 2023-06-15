28318a33f1d72bbe3d90a98bd3122fa292fc0752
healthlens <janani.meiiporul@gmail.com>
2023-06-15 15:23:58 -0400

    Zipchange

--- carecadet/src/Pages/Home/patienthomepage.tsx
+++ carecadet/src/Pages/Home/patienthomepage.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect, useState } from "react";
 import { Formik, Form, ErrorMessage, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import healthcare from "../../Images/healthcare.jpg";
 import SearchIcon from "@mui/icons-material/Search";
-import { NavLink } from "react-router-dom";
+
 
 import {
   Grid,
@@ -54,35 +54,69 @@ interface forminitialValues {
 }
 
 
-const Patienthomepage = () => {
+export default function Patienthomepage(){
+ 
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const [checkInfo, setCheckInfo] = useState<any>([])
-  const [locaInfo, setLocaInfo] = useState<any>([])
+  const [locaInfo, setLocaInfo] = useState<any>()
+  const[userLoc,setUserLoc] =useState<any>({lat:"",lon:""})
+  console.log("userLoc",userLoc)
+  const [zipchange,setZipChange] = useState<any>()
+  console.log("zipchange",zipchange)
   const OPTIONS_LIMIT = 10;
   const defaultFilterOptions = createFilterOptions();
+  // const[lat,setLatitude] = useState<number>();
+  // const [lon,setLongitude] = useState<any>();
+  console.log("locaInfo",locaInfo)
+ const valRef = useRef("");
 
-//   const[lat,setLatitude] = useState<number>();
-//   const [lon,setLongitude] = useState<any>();
-  
-//   console.log("locaInfo",locaInfo)
 
 
-//        navigator.geolocation.getCurrentPosition((position) =>{
-//      setLatitude(position.coords.latitude);
-//      setLongitude(position.coords.longitude)
-//      const zipCode = {lat,lon}
-//      console.log("zipCode",zipCode)
-//      axiosPrivate.post(`/search/serviceLocationSearch`,zipCode)
-//    .then((res)=>{
-// setLocaInfo(res.data.data)
-//    })
-//    .catch((e)=>console.log(e));
-//     })
+function errorHandler(err:any) {
+  if(err.code == 1) {
+     alert("Error: Access is denied!");
+  } else if( err.code == 2) {
+     alert("Error: Position is unavailable!");
+  }
+}
+function getLocationUpdate(){
+  if(navigator.geolocation){
+     navigator.geolocation.watchPosition((position) =>
+       {
+     const Lat=position.coords.latitude;
+     const Lon=position.coords.longitude;
+     const zipCode1 = {lat:Lat,lon:Lon}
+     console.log("zipCode1",zipCode1)
+     axiosPrivate.post(`/search/serviceLocationSearch`,zipCode1)
+   .then((res)=>{
+    console.log("....testtttt")
+  setLocaInfo(res.data.data)
+// valRef.current = locaInfo
+   })
+   .catch((e)=>console.log(e));
+    })
   
-  
-  // console.log(lat,'latitude');
-  // console.log(lon,'longitude');
+ 
+    
+  } 
+  else {
+    alert("sorry,browser does not support geolocation")
+  }
+}
+
+
+
+
+
+    
+useEffect(()=>{
+  // navigator.geolocation.watchPosition(showLocation,errorHandler)
+  getLocationUpdate();
+ },[])
+
+
+
 
   const filterOptions = (options: any, state: any) => {
     return defaultFilterOptions(options, state).slice(0, OPTIONS_LIMIT);
@@ -92,13 +126,14 @@ const Patienthomepage = () => {
 
   const initialValues: forminitialValues = {
     Service: "",
-    Location: ""
+    Location: locaInfo
   };
   const validationSchema = Yup.object().shape({
     Service: Yup.string().required("Required"),
     Location: Yup.string().required("Required")
   }); 
   const onSubmit = (values: forminitialValues, actions: any) => {
+    console.log("testsubmit")
    dispatch(dataQuery(values))
     navigate(`/patient/search?q=${values.Service.trim()}&location=${values.Location.trim()}`);
     // axiosPrivate.get(`http://210.18.155.251:5003/search/?q=${values.Service}&location=${values.Location}`)
@@ -118,6 +153,7 @@ const Patienthomepage = () => {
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
+      enableReinitialize
     >
        {({ handleChange, setFieldValue, values }) => (
       <Form>
@@ -154,6 +190,7 @@ const Patienthomepage = () => {
                     filterOptions={filterOptions}
                     freeSolo
                     onInputChange={(e: any, value: any) => {
+                   
                     const postData = { q:value }
                     console.log(value,'onchangevalue')
                     setFieldValue("Service", value);
@@ -197,23 +234,16 @@ const Patienthomepage = () => {
                  
            
                 </Grid>
+              
                 <Grid item xs={12} md={4}>
                 <Field
                     as={TextField}
                     name="Location"
-                    // value= {locaInfo}
-                    placeholder="Location"
-                    type="text"
-                    // onChange={((e:any,value:any)=>{
-                    //   if(!locaInfo){
-                    //   e.target.value
-                    //   console.log(e.target.value,".............k")}
-                    //   else{
-                    //     value={locaInfo}
-                    //   }
-                    //                     })}
-           
-                    fullWidth={true}
+                    value={values.Location}
+                                 
+                type="text"
+                  
+                       fullWidth={true}
                     sx={{
                       borderRadius: 1,
                       "&::placeholder": {
@@ -232,6 +262,7 @@ const Patienthomepage = () => {
                       },
                     }}
                   />
+                    
                 </Grid>
               </Grid>
 
@@ -306,223 +337,9 @@ const Patienthomepage = () => {
         </Box>
       </Form>)}
     </Formik>
-    // </Box>
-    // <Box
-    //   sx={{
-    //     background: "transparent",
-    //   }}
-    // >
-    //   <Formik
-    //     initialValues={initialValues}
-    //     validationSchema={validationSchema}
-    //     onSubmit={onSubmit}
-    //   >
-    //     <Form>
-    //         {/* <Box
-    //               sx={{
-    //                 padding:"1rem",
-    //                 display: "flex",
-    //                 justifyContent:"center",
-    //                 alignItems:"center",
-    //                 background: "#4D77FF",
-    //                 height: "6em",
-    //                 width: "55em",
-    //                 gap:"1rem"
 
-    //               }}
-    //             >
-    //               <FormTextField
-    //                 container={TextField}
-
-    //                 name="Service"
-    //                 placeholder="Search Service"
-    //                 type="text"
-    //                 fullWidth={false}
-    //                 sx={{
-    //                  borderRadius:1,
-    //                   ".MuiInputBase-input" : {
-    //                     background: "white"
-    //                   },
-    //                   ".MuiFormLabel-root ": {
-    //                     letterSpacing: "0.2rem",
-    //                     fontSize: "0.8rem",
-    //                   },
-    //                   ".MuiInputLabel-shrink": {
-    //                     letterSpacing: 0,
-    //                   },
-    //                 }}
-    //               />
-
-    //               <FormTextField
-    //                 container={TextField}
-
-    //                 name="Location"
-    //                 placeholder="location"
-    //                 type="text"
-    //                 fullWidth={false}
-    //                 sx={{
-    //                   borderRadius:1,
-    //                   ".MuiInputBase-input" : {
-    //                     background: "white"
-    //                   },
-    //                   ".MuiFormLabel-root ": {
-    //                     letterSpacing: "0.2rem",
-    //                     fontSize: "0.8rem",
-    //                   },
-    //                   ".MuiInputLabel-shrink": {
-    //                     letterSpacing: 0,
-    //                   },
-    //                 }}
-    //               />
-    //             </Box> */}
-    //       <Box
-    //         sx={{
-    //           width: "100%",
-    //           // height: "160vh",
-    //           backgroundColor: "primary.light",
-    //         }}
-    //       >
-    //         <Grid
-    //           container
-    //           spacing={3.5}
-    //           direction="row"
-    //           // justifyContent="flex-start"
-    //           // alignItems="flex-start"
-    //           // sx={{ ml: "10px" }}
-    //         >
-
-    //         <Grid item xs= {12} md={7}>
-
-    //             <Grid container xs={12} sx={{
-    //                 padding:"1rem",                    
-    //                 background: "#4D77FF",                   
-    //                 // width: "55em",
-    //                 gap:"1.5rem"
-
-    //               }}>
-    //                 <Grid item xs={12} md={7.5} >
-    //                 <FormTextField
-    //                 container={TextField}                   
-    //                 name="Service"
-    //                 placeholder="Search Service"
-    //                 type="text"
-    //                 fullWidth={false}
-    //                 inputProps={{ sx: { height: 500 } }}
-    //                 sx={{
-    //                  borderRadius:1,
-    //                   ".MuiInputBase-input" : {
-    //                     background: "white"
-    //                   },
-    //                   ".MuiFormLabel-root ": {
-    //                     letterSpacing: "0.2rem",
-    //                     fontSize: "0.8rem",
-
-    //                   },
-    //                   ".MuiInputLabel-shrink": {
-    //                     letterSpacing: 0,
-    //                   },
-    //                   '&::placeholder': {
-    //                     fontSize:"1.3rem",
-    //                      color: 'black'
-    //                    }
-
-    //                 }}
-    //               />
-    //                 </Grid>
-    //                 <Grid  item xs ={12} md={4}>
-    //                 <FormTextField
-    //                 container={TextField}                   
-    //                 name="Location"
-    //                 placeholder="Location"
-    //                 type="text"
-    //                 fullWidth={false}
-    //                 sx={{
-    //                   borderRadius:1,
-    //                   ".MuiInputBase-input" : {
-    //                     background: "white"
-    //                   },
-    //                   ".MuiFormLabel-root ": {
-    //                     letterSpacing: "0.2rem",
-    //                     fontSize: "0.8rem",
-    //                   },
-    //                   ".MuiInputLabel-shrink": {
-    //                     letterSpacing: 0,
-    //                   },
-    //                   '&::placeholder': {
-    //                     fontSize:"1.3rem",
-    //                      color: 'black'
-    //                    }
-    //                 }}/>
-    //                 </Grid>                    
-    //                 </Grid>
-    //                 <Buttoncomponent
-    //             type="submit"
-    //             size="large"
-    //             fullWidth={false}
-    //             variant="contained"
-    //             sx={{
-    //               marginTop: "-100px",
-    //               ml: "350px",
-    //               backgroundColor: "secondary.dark",
-    //               // width: "20vw",
-    //               color: "#fff",
-    //               display: "flex",
-    //               justifyContent: "center",
-    //               // gap:"1.2rem",
-
-    //               "&:hover": {
-    //                 color: "secondary.dark",
-    //                 border: "1px solid blue",
-    //                 // letterSpacing: "0.2rem",
-    //                 // fontSize: "1rem",
-    //               },
-    //             }}
-    //           >
-    //             <SearchIcon /> Find care
-    //           </Buttoncomponent>
-    //           </Grid>
-
-    //           <Grid item xs={3.75} sx={{ mt: "-250px" , display:{xs:"none" , md:'block'}}}>
-    //             <img
-    //               src={healthcare}
-    //               alt="Home"
-    //               style={{
-    //                 width: "650px",
-    //                 height: "530px",
-    //                 //  top: "35px",
-    //                 // right: "60%",
-    //                 borderRadius: "13px",
-    //               }}
-    //             />
-
-    //           </Grid>
-
-
-    //         </Grid>
-
-    //         <Box
-    //           sx={{
-    //             backgroundColor: "RGB(217 229 251)",
-    //             height: "10em",
-    //             mt: "50px",
-    //           }}
-    //         >
-    //           <Box
-    //             sx={{
-    //               display: "flex",
-    //               fontWeight: "bold",
-    //               fontSize: "30px",
-    //               padding: "50px",
-    //             }}
-    //           >
-    //             Care<Box sx={{ color: "#4D77FF" }}>Cadet</Box>
-    //           </Box>
-    //         </Box>
-    //       </Box>
-    //     </Form>
-    //   </Formik>
-    // // </Box>
   );
 };
 
-export default Patienthomepage;
+
+
