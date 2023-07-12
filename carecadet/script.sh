658b0596239b5ea0f511a4b938de746d14f9bc67
Divya <divyag.meiiporul@gmail.com>
2023-07-12 14:58:12 +0530

    US server test

--- carecadet/script.sh
+++ carecadet/script.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
-docker stop healthlens_react_app
-docker rm healthlens_react_app
+docker stop healthlens_react_app1
+docker rm healthlens_react_app1
 # docker rmi healthlens_react_app --force
-docker build -t healthlens_react_app .
-docker run -p 5008:3000 -d --name healthlens_react_app --restart always healthlens_react_app
+docker build -t healthlens_react_app1 .
+docker run -p 5008:3000 -d --name healthlens_react_app1 --restart always healthlens_react_app1
